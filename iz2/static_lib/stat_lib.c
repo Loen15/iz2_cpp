323c024fed2d8d7b9c4ec0e5b1bf8292f673a338
Loen15 <59060743+Loen15@users.noreply.github.com>
2022-03-30 00:47:40 +0300

    .

---
+++ iz2/static_lib/stat_lib.c
@@ -0,0 +1,5 @@
+//
+// Created by Иван on 30.03.2022.
+//
+
+#include "stat_lib.h"
---
+++ iz2/static_lib/stat_lib.h
@@ -0,0 +1,8 @@
+//
+// Created by Иван on 30.03.2022.
+//
+
+#ifndef IZ2_STAT_LIB_H
+#define IZ2_STAT_LIB_H
+
+#endif //IZ2_STAT_LIB_H
