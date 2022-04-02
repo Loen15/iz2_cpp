9d25a579673f04b3a9760a6b9e5a0aa8c329f740
Loen15 <59060743+Loen15@users.noreply.github.com>
2022-04-02 06:13:51 +0300

    cpplint edit

--- iz2/main.c
+++ iz2/main.c
@@ -3,5 +3,5 @@
 #include "dinamic_lib/din_lib.h"
 
 int main() {
-    return start("/iz2_cpp/iz2/predicat.txt","/iz2_cpp/iz2/variables.txt");
+    return start("/iz2_cpp/iz2/predicat.txt", "/iz2_cpp/iz2/variables.txt");
 }
