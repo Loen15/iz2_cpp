7b7a9f9d3c3bfd461cbf3e305fb00511d9945913
Loen15 <59060743+Loen15@users.noreply.github.com>
2022-04-02 04:44:19 +0300

    change PATH in stat_tests

--- iz2/tests/stat_tests.cpp
+++ iz2/tests/stat_tests.cpp
@@ -5,10 +5,10 @@ extern "C" {
 }
 
 TEST(start,correct){
-    std::string correct_predicat = "/iz2_cpp/iz2/predicat.txt";
+    std::string correct_predicat = "../iz2_cpp/iz2/predicat.txt";
     char *cor_pred = new char[correct_predicat.length() + 1];
     strcpy(cor_pred, correct_predicat.c_str());
-    std::string correct_var = "/iz2_cpp/iz2/variables.txt";
+    std::string correct_var = "../iz2_cpp/iz2/variables.txt";
     char *cor_var = new char[correct_var.length() + 1];
     strcpy(cor_var, correct_var.c_str());
     EXPECT_EQ(6, start(cor_pred,cor_var));
